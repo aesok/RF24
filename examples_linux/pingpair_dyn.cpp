0787955ce22a07468514b1602c37620d4db40c3f
Anatoly Sokolov <aesok@post.ru>
2016-06-04 15:44:09 +0000

    Fix examples in examples_linux directory.

--- examples_linux/pingpair_dyn.cpp
+++ examples_linux/pingpair_dyn.cpp
@@ -32,7 +32,7 @@ using namespace std;
 //RF24 radio(RPI_BPLUS_GPIO_J8_15,RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ);
 
 // Setup for GPIO 15 CE and CE0 CSN with SPI Speed @ 8Mhz
-RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 
 /*** RPi Alternate ***/
 //Note: Specify SPI BUS 0 or 1 instead of CS pin number.
@@ -41,8 +41,9 @@ RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 //RPi Alternate, with MRAA
 //RF24 radio(15,0);
 
-//RPi Alternate, with SPIDEV - Note: Edit RF24/arch/BBB/spi.cpp and  set 'this->device = "/dev/spidev0.0";;' or as listed in /dev
-//RF24 radio(22,0);
+//RPi Alternate, with SPIDEV.
+SPI spi ("/dev/spidev1.0");
+RF24 radio (spi, 22);
 
 
 /****************** Linux (BBB,x86,etc) ***********************/
